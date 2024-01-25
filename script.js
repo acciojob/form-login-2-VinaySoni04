a4f7a19fda8b0a5ecee337ed976da001b1b51657
Vinay Soni <98009479+VinaySoni04@users.noreply.github.com>
2024-01-25 13:27:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-//your JS code here. If required.
+function submitForm(){
+	var firstName=document.getElementById('firstname').value;
+	var lastName=document.getElementById('lastname').value;
+	var phoneNumber=document.getElementById('phone').value;
+	var email=document.getElementById('email').value;
+	var result= "First Name: " + firstName + "\n" +
+                 "Last Name: " + lastName + "\n" +
+                 "Phone Number: " + phoneNumber + "\n" +
+                 "Email ID: " + email;
+	alert(result);
+}
